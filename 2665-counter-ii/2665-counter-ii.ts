36a88f3ab60a2b824071b6eb00d039058e388401
Joydeep Das <joydeep.accounts@tuta.io>
2026-05-09 21:24:13 +0530

    Time: 43 ms (90.18%), Space: 58.5 MB (23.68%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.ts
@@ -0,0 +1,27 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+
+    let val = init;
+
+    return {
+        increment: () => ++val,
+        decrement: () => --val,
+        reset: () => {
+            val = init;
+            return init;
+        },
+    };
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
