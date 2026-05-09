e5a3024d21b744ee720cb314a662f7ffa03447e8
Joydeep Das <joydeep.accounts@tuta.io>
2026-05-09 21:12:47 +0530

    Time: 0 ms (100%), Space: 55.9 MB (13.79%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.ts
@@ -0,0 +1,13 @@
+function hasAlternatingBits(n: number): boolean {
+
+    const binary = n.toString(2);
+
+    for( let i = 0; i < binary.length - 1; i++ ) {
+        if( binary[i] === binary[i + 1] ) {
+            return false;
+        }
+    }
+
+    return true;
+    
+};
